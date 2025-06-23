e66f963dc08f2d27737f8778906ee4e6a289aed1
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-06-23 09:44:17 +0530

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
+// src/app/services/auth.service.ts
+
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const API_URL = 'http://13.127.72.112:8282/api/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${API_URL}/register`, data);
+  }
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${API_URL}/login`, data);
+  }
 }
